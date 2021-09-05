85a3c09f0d037c42444adb5b0c8e5123b15568e4
Michael Rao <jmichaelrao@gmail.com>
2021-09-05 17:54:28 -0500

    updated category dropdown with bootstrap removed so that logic works

--- client/src/components/CategoryDropdown.js
+++ client/src/components/CategoryDropdown.js
@@ -7,45 +7,51 @@ const CategoryDropdown = () => {
   const [items, setItems] = React.useState([
     { label: "Loading...", value: "" },
   ]);
+  // controll the dropdown value with state
   const [value, setValue] = React.useState();
-  // Some guidelines for creating a dropdown from an API response
-  // https://www.carlrippon.com/drop-down-data-binding-with-react-hooks/
-  // https://api.windy.com/api/webcams/v2/list?show=categories&key=2LUGQul9yRztrjwRIjsxvu1laj1HUCuq
 
   // consider possibly moving this to the API.js page in Utils
   React.useEffect(() => {
+    // abort loading when unmounted to avoid error on fast page redirect
+    let unmounted = false;
+    // get categories from API list
     async function getWebcamCategories() {
       const response = await fetch(
         `https://api.windy.com/api/webcams/v2/list?show=categories&key=2LUGQul9yRztrjwRIjsxvu1laj1HUCuq`
       );
       const body = await response.json();
-      setItems(
-        body.result.categories.map(({ name }) => ({ label: name, value: name }))
-      );
-      setLoading(false);
+      if (!unmounted) {
+        setItems(
+          body.result.categories.map(({ name }) => ({
+            label: name,
+            value: name,
+          }))
+        );
+        setLoading(false);
+      }
     }
     getWebcamCategories();
+    return () => {
+      unmounted = true;
+    };
   }, []);
 
   return (
-    <Container>
-      <DropdownButton
+    // redo all of this with bootstrap components... unless you're going to break it :)
+    <>
+      <select
         disabled={loading}
-        className="text-dark m-2"
-        variant="light"
-        size="lg"
-        title="Categories"
         value={value}
         onChange={(e) => setValue(e.currentTarget.value)}
       >
+        <option value="null">--Choose a Category--</option>
         {items.map(({ label, value }) => (
-          <Dropdown.Item key={value} value={value}>
+          <option key={value} value={value}>
             {label}
-          </Dropdown.Item>
+          </option>
         ))}
-      </DropdownButton>
-      <div>Selected Category: {value}</div>
-    </Container>
+      </select>
+    </>
   );
 };
 
