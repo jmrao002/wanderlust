17476a1994b69894f1ff17872e5544d2b1a228d0
Michael Rao <jmichaelrao@gmail.com>
2021-09-05 12:45:45 -0500

    messing around with some category dropdown selection logic to try to get either the category title to change or for a new field to be populated with data upon selection

--- client/src/components/CategoryDropdown.js
+++ client/src/components/CategoryDropdown.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Container, Dropdown, DropdownButton } from "react-bootstrap";
-import { getWebcamCategories } from "../utils/API";
 
 const CategoryDropdown = () => {
-    const [items, setItems] = React.useState([]);
-    const [value, setValue] = React.useState();
+  const [loading, setLoading] = React.useState(true);
+  const [items, setItems] = React.useState([
+    { label: "Loading...", value: "" },
+  ]);
+  const [value, setValue] = React.useState();
   // Some guidelines for creating a dropdown from an API response
   // https://www.carlrippon.com/drop-down-data-binding-with-react-hooks/
   // https://api.windy.com/api/webcams/v2/list?show=categories&key=2LUGQul9yRztrjwRIjsxvu1laj1HUCuq
 
+  // consider possibly moving this to the API.js page in Utils
   React.useEffect(() => {
     async function getWebcamCategories() {
       const response = await fetch(
@@ -18,6 +21,7 @@ const CategoryDropdown = () => {
       setItems(
         body.result.categories.map(({ name }) => ({ label: name, value: name }))
       );
+      setLoading(false);
     }
     getWebcamCategories();
   }, []);
@@ -25,6 +29,7 @@ const CategoryDropdown = () => {
   return (
     <Container>
       <DropdownButton
+        disabled={loading}
         className="text-dark m-2"
         variant="light"
         size="lg"
@@ -38,6 +43,7 @@ const CategoryDropdown = () => {
           </Dropdown.Item>
         ))}
       </DropdownButton>
+      <div>Selected Category: {value}</div>
     </Container>
   );
 };
